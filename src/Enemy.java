e46fcc0eed6ee51c30945a9bd7ebecb29428b173
jamestcorbett <jcorbett@holyghostprep.org>
2021-12-07 13:28:09 -0500

    just comments -- details for stuff due thursday in comments

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,7 @@
 import java.util.Random;
+//30 points is stage 1, and the fighting mechanics
+//shop,world, enemies, encounters, 2 other interesting things is monday.
+//3:30 on thursday
 
 public class Enemy {
     private int health;
@@ -12,7 +15,10 @@ public class Enemy {
         this.attackPower = 10;
         this.type = "fire";
     }
-
+//    public class Enemy1{
+//        private int health;
+//        private int attackPower
+//    }
     void attack(Hero h) {    //did this in hero class line 21. idk if this should still be here or not - james
         Random r = new Random();
         // 20% of the time it is a CRITICAL HIT, 2 * AP
